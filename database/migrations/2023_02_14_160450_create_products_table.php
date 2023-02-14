b424b7ac9a328d19a55b135672d67b99f318cb34
gdemartino93 <113302882+gdemartino93@users.noreply.github.com>
2023-02-14 17:23:33 +0100

    create migrations

---
+++ database/migrations/2023_02_14_160450_create_products_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+
+            $table -> string('name',32);
+            $table -> string('code', 5);
+            $table -> string('description' , 80);
+            $table -> integer('price',false,true);
+            $table -> integer('height',false,false);
+            
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+};
