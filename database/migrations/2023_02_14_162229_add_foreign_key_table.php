e78b1bc6401431fe75e238daa19676b785d6c2c6
gdemartino93 <113302882+gdemartino93@users.noreply.github.com>
2023-02-14 21:53:26 +0100

    fix seed

--- database/migrations/2023_02_14_162229_add_foreign_key_table.php
+++ database/migrations/2023_02_14_162229_add_foreign_key_table.php
@@ -17,8 +17,8 @@ return new class extends Migration
             $table -> foreignId('typology_id') -> constrained();
         });
         Schema::table('category_product', function (Blueprint $table) {
-            $table -> foreignId('categories_id') -> constrained();
-            $table -> foreignId('products_id') -> constrained();
+            $table -> foreignId('category_id') -> constrained();
+            $table -> foreignId('product_id') -> constrained();
 
         });
         
@@ -37,8 +37,16 @@ return new class extends Migration
         });
         Schema::table('category_product', function (Blueprint $table) {
             $table -> dropForeign('category_product_categories_id_foreign');
-            
+            // $table -> dropColumn('category_id');
+
             $table -> dropForeign('category_product_products_id_foreign');
+            // $table -> dropColumn('product_id');
+
+            // $table -> dropForeign('category_product_categories_id_foreign');
+            // $table -> dropColumn('categories_id');
+
+            // $table -> dropForeign('category_product_products_id_foreign');
+            // $table -> dropColumn('products_id');
         });
         
     }
