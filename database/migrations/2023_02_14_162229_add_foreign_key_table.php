0b65ccd10416dab55d4d6c8dfbaf8eca692dc705
gdemartino93 <113302882+gdemartino93@users.noreply.github.com>
2023-02-14 20:59:54 +0100

    fix dropforeing need drop column

--- database/migrations/2023_02_14_162229_add_foreign_key_table.php
+++ database/migrations/2023_02_14_162229_add_foreign_key_table.php
@@ -33,20 +33,12 @@ return new class extends Migration
     {
         Schema::table('products', function (Blueprint $table) {
             $table -> dropForeign('products_typology_id_foreign');
-            $table -> dropColumn('typology_id');
+            // $table -> dropColumn('typology_id');
         });
         Schema::table('category_product', function (Blueprint $table) {
-            $table -> dropForeign('category_product_category_id_foreign');
-            $table -> dropColumn('category_id');
-
-            $table -> dropForeign('category_product_product_id_foreign');
-            $table -> dropColumn('product_id');
-
             $table -> dropForeign('category_product_categories_id_foreign');
-            $table -> dropColumn('categories_id');
-
+            
             $table -> dropForeign('category_product_products_id_foreign');
-            $table -> dropColumn('products_id');
         });
         
     }
