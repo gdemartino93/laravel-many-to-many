d5d8e776e627af7c4524d3a67326aa3eb4465373
gdemartino93 <113302882+gdemartino93@users.noreply.github.com>
2023-02-14 22:05:07 +0100

    drop fk and column on rollback

--- database/migrations/2023_02_14_162229_add_foreign_key_table.php
+++ database/migrations/2023_02_14_162229_add_foreign_key_table.php
@@ -31,22 +31,19 @@ return new class extends Migration
      */
     public function down()
     {
+        // droppo le fk e le colonne della table product quando eseguo il rollback
         Schema::table('products', function (Blueprint $table) {
             $table -> dropForeign('products_typology_id_foreign');
-            // $table -> dropColumn('typology_id');
+            $table -> dropColumn('typology_id');
         });
+        // droppo le fk e le colonne della table category_product quando eseguo il rollback
         Schema::table('category_product', function (Blueprint $table) {
             $table -> dropForeign('category_product_category_id_foreign');
-            // $table -> dropColumn('category_id');
+            $table -> dropColumn('category_id');
 
             $table -> dropForeign('category_product_product_id_foreign');
-            // $table -> dropColumn('product_id');
+            $table -> dropColumn('product_id');
 
-            // $table -> dropForeign('category_product_categories_id_foreign');
-            // $table -> dropColumn('categories_id');
-
-            // $table -> dropForeign('category_product_products_id_foreign');
-            // $table -> dropColumn('products_id');
         });
         
     }
