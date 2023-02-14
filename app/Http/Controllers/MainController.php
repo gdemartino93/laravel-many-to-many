a4ba1356e7deeeda6c33667417b7c3e9f1625e30
gdemartino93 <113302882+gdemartino93@users.noreply.github.com>
2023-02-15 00:30:41 +0100

    create home page

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -3,13 +3,20 @@
 namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 Use App\Models\Product;
+use App\Models\Category;
 
 class MainController extends Controller
 {
     public function home(){
+        $categories = Category :: all();
+        // dd($categories);
+        return view('pages.home', compact("categories"));
+    }
+
+    public function products(){
        
         $products = Product :: all();
         // dd($products);
-        return view('pages.home', compact('products'));
+        return view('pages.products', compact('products'));
     }
 }
